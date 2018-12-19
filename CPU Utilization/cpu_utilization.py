f214d147ec098358a0b1231ef7ce4867ac8453b6
Lawrence37 <45837045+Lawrence37@users.noreply.github.com>
2018-12-19 15:46:22 -0800

    Updated header of cpu_utilization.py

--- CPU Utilization/cpu_utilization.py
+++ CPU Utilization/cpu_utilization.py
@@ -15,8 +15,9 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 
-
+'''
 Prints the (near) instantaneous core/CPU utilization as percentages of user,
 nice, system, idle, and wait loads. Utilization is the percentage of the core or
 cpu's maximum processing capacity given the number of active cores and maximum
